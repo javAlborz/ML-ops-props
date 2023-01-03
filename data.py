1081c9bbbd703f56d4fa27a90748cc367a24cb7e
javAlborz <alborz.sa@gmail.com>
2023-01-03 11:22:48 +0100

    changed in the right directory

--- data.py
+++ data.py
@@ -1,1 +1,9 @@
-np.load
+
+import torch
+
+
+def mnist():
+    # exchange with the corrupted mnist dataset
+    train = torch.randn(50000, 784)
+    test = torch.randn(10000, 784) 
+    return train
